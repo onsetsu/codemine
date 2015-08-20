df7844e416d660fb7d7bd99036519b4e46c1afdf
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-08-20 12:07:30 +0200

    estools AST traversal visitor

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import * as esprima from 'esprima'
+"use strict";
 
-console.log(esprima);
-console.log(esprima.parse(`/*
+class WordIndexMapper {
+    constructor() {
+        this.indexByWord = new Map();
+        this.wordByIndex = new Map();
+    }
+
+    hasWord(word) {
+        return this.indexByWord.has(word);
+    }
+}
+
+import * as esprima from 'esprima'
+import * as estools from 'estools'
+import * as gulp from 'gulp'
+
+var testAST = esprima.parse(`/*
  * Life, Universe, and Everything
  */
 function answer() {
@@ -12,4 +26,22 @@ function answer() {
     // this is after the return O_o
 }
 
-answer(6, 7);`));
+answer(6, 7);`);
+
+console.log(testAST);
+
+estools.traverse(testAST, {
+    enter: function(node, parent) {
+        switch(node.type) {
+            case 'Literal':
+                console.log(node.type, node.value);
+                break;
+            case 'Identifier':
+                console.log(node.type, node.name);
+                break;
+            default:
+                console.log(node.type);
+                break;
+        }
+    }
+});
