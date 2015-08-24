8deb7485312a90d1d17a65c8b48803f590887e7e
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-08-24 15:06:32 +0200

    added gulp task to apply a given function on matching files

---
+++ gulp-codemine.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+// consts
+const PLUGIN_NAME = 'gulp-codemine';
+
+// plugin level function (dealing with files)
+function applyFunction(fn) {
+    if (!fn) {
+        throw new PluginError(PLUGIN_NAME, 'Missing function!');
+    }
+
+    // creating a stream through which each file will pass
+    var stream = through.obj(function(file, enc, cb) {
+        fn(file.contents.toString(enc));
+
+        cb(null, file);
+        // or:
+        // make sure the file goes through the next gulp plugin
+        // this.push(file);
+        // tell the stream engine that we are done with this file
+        // cb();
+    });
+
+    // returning the file stream
+    return stream;
+}
+
+// exporting the plugin main function
+module.exports = applyFunction;
