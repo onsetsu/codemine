8eb031466d14a6fe7e02eaaea4fe9d5ba1d822bc
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-01 18:18:19 +0200

    refactored test for LDDocumentFeatureSpace

--- test/lda/lddocumentfeaturespace-test.js
+++ test/lda/lddocumentfeaturespace-test.js
@@ -1,47 +1,49 @@
 "use strict";
 
 import LDDocumentFeatureSpace from './../../src/lda/lddocumentfeaturespace';
-var assert = require("assert");
+import assert from 'assert';
 
 describe('LDA', function() {
     describe('LDDocumentFeatureSpace', function () {
+        var firstDocument = ['foo', 'bar', 'blub'],
+            secondDocument = ['bar', 'bar', 'blub', 'baz'],
+            featureSpace;
+
+        beforeEach(() => {
+            featureSpace = new LDDocumentFeatureSpace();
+        });
+
         describe('represent/representAll', function () {
             it('should return the indices representing the given words', function () {
-                var space = new LDDocumentFeatureSpace();
-
-                assert.deepEqual([0, 1, 2], space.representAll(['foo', 'bar', 'blub']));
-                assert.deepEqual([1, 1, 2, 3], space.representAll(['bar', 'bar', 'blub', 'baz']));
+                assert.deepEqual([0, 1, 2], featureSpace.representAll(firstDocument));
+                assert.deepEqual([1, 1, 2, 3], featureSpace.representAll(secondDocument));
 
                 // show mapping of features to ids
-                //space.featureIdMap.forEach((value, key) => console.log(key, '>', value));
-                //space.idFeatureMap.forEach((value, key) => console.log(key, '>', value));
+                //featureSpace.featureIdMap.forEach((value, key) => console.log(key, '>', value));
+                //featureSpace.idFeatureMap.forEach((value, key) => console.log(key, '>', value));
 
                 // show entries in document-word matrix
-                //space.documentFeatureCounts.forEach((value, key) => console.log(key, value));
+                //featureSpace.documentFeatureCounts.forEach((value, key) => console.log(key, value));
             });
         });
 
         describe('interpret/interpretAll', function () {
             it('should return the indices previously saved', function () {
-                var space = new LDDocumentFeatureSpace();
+                featureSpace.representAll(firstDocument);
+                featureSpace.representAll(secondDocument);
 
-                space.representAll(['foo', 'bar', 'blub']);
-                space.representAll(['bar', 'bar', 'blub', 'baz']);
-
-                assert.equal('bar', space.interpret(1));
-                assert.deepEqual(['foo', 'baz'], space.interpretAll([0, 3]));
+                assert.equal('bar', featureSpace.interpret(1));
+                assert.deepEqual(['foo', 'baz'], featureSpace.interpretAll([0, 3]));
             });
         });
 
         describe('featureCount', function () {
             it('should return the number of different features contained by the documents', function () {
-                var space = new LDDocumentFeatureSpace();
-
-                assert.equal(0, space.featureCount());
-                space.representAll(['foo', 'bar', 'blub']);
-                assert.equal(3, space.featureCount());
-                space.representAll(['bar', 'bar', 'blub', 'baz']);
-                assert.equal(4, space.featureCount());
+                assert.equal(0, featureSpace.featureCount());
+                featureSpace.representAll(firstDocument);
+                assert.equal(3, featureSpace.featureCount());
+                featureSpace.representAll(secondDocument);
+                assert.equal(4, featureSpace.featureCount());
             });
         });
     });
