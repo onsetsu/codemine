e7b8b3fe91868c14205718c310803bf210c67c90
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-01 16:57:44 +0200

    wrote test for utils

---
+++ test/utils-test.js
@@ -0,0 +1,11 @@
+// testing sumItems
+var sumItems = require('./../src/utils').sumItems;
+var assert = require("assert");
+
+describe('utils', function() {
+    describe('sumItems', function () {
+        it('should sum up the numbers in the given Array', function () {
+            assert.equal(76, sumItems([1,4,68,3]));
+        });
+    });
+});
