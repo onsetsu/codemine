ffb2ad912d6c6befed2f7a28a0dfb2c40b1558fc
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-02 13:41:49 +0200

    added test for range functionality

--- test/utils-test.js
+++ test/utils-test.js
@@ -2,7 +2,8 @@
 
 var utils = require('./../src/utils'),
     sumItems = utils.sumItems,
-    zipMap = utils.zipMap;
+    zipMap = utils.zipMap,
+    range = utils.range;
 var assert = require("assert");
 
 describe('utils', function() {
@@ -11,10 +12,16 @@ describe('utils', function() {
             assert.equal(76, sumItems([1,4,68,3]));
         });
     });
-    
+
     describe('zipMap', function () {
         it('should invoke the given map function with a pair of same-indexed elements', function () {
             assert.deepEqual([7, 9, 11], zipMap([1,2,3,4,5], [6,7,8], (a, b) => a + b));
+        });
+    });
+
+    describe('range', function () {
+        it('should an Array containing natural numbers up to the given number', function () {
+            assert.deepEqual([0, 1, 2, 3, 4, 5], range(6));
         });
     });
 });
