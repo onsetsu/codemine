e0055b7d82159540314420738fc62c24ab4153fb
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-09-02 14:12:50 +0200

    extracted LDModel

--- src/index.js
+++ src/index.js
@@ -49,49 +49,7 @@ gulp.start('codemine');
 console.log('------------ LDA ------------');
 
 var Matrix = require('./math/matrix').Matrix;
-
-var cosineSimilarity = require('./math/utils');
-
-class LDModel {
-    documentSimilarityOfAnd(doc1, doc2) {
-        return cosineSimilarity(
-            this.topicsForDocumentAt(doc1),
-            this.topicsForDocumentAt(doc2)
-        );
-    }
-
-    documentsForTopicAt(topic) {
-        return this.perDocumentTopics.atColumn(topic);
-    }
-
-    featureSimilarityOfAnd(word1, word2) {
-        return cosineSimilarity(
-            this.topicsForFeatureAt(word1),
-            this.topicsForFeatureAt(word2)
-        );
-    }
-
-    featuresForTopicAt(topic) {
-        return this.perTopicFeatures.atRow(topic);
-    }
-
-    initializeTopicsAndDocumentsAndPriors(topics, documents, priors) {
-        //console.log(topics);
-        this.perTopicFeatures = topics;
-        this.perDocumentTopics = documents;
-        this.topicPriors = priors;
-
-        return this;
-    }
-
-    topicsForDocumentAt(docid) {
-        return this.perDocumentTopics.atRow(docid);
-    }
-
-    topicsForFeatureAt(word) {
-        return this.perTopicFeatures.atColumn(word);
-    }
-}
+import LDModel from './lda/model';
 
 class LDAllocator {
     allocateIteratingFeaturesTopics(docs, numIterations, numFeatures, numTopics) {
